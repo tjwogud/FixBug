8b9e2bcb1c9a737eefb689b10982a73b7afba066
tjwogud <rlatjsal78@gmail.com>
2022-03-14 16:55:35 +0900

    add function

---
+++ Patch/LateUpdatePatch.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace FixBug.Patch
+{
+    [HarmonyPatch(typeof(scnEditor), "Start")]
+    public static class LateUpdatePatch
+    {
+        public static void Postfix()
+        {
+            scnEditor.instance.gameObject.AddComponent<VolumeSetter>();
+        }
+
+        public class VolumeSetter : MonoBehaviour
+        {
+            public void LateUpdate()
+            {
+                float num = scrController.volume * 0.1f;
+                AudioListener.volume = 0.5f * num * num;
+            }
+        }
+    }
+}
