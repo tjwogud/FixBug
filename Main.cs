5482eba9e6d473b5b2d5088915563fe2b95c73ab
tjwogud <rlatjsal78@gmail.com>
2022-03-14 16:58:33 +0900

    remove something

--- Main.cs
+++ Main.cs
@@ -48,12 +48,6 @@ namespace FixBug
                 scnEditor.instance?.Set("forceQuit", true);
                 Application.Quit();
             }
-            if (GUILayout.Button("테스트", GUILayout.Width(150)))
-            {
-                Logger.Log($"playMode: {scnEditor.instance.playMode}");
-                Logger.Log($"userIsEditingAnInputField: {scnEditor.instance.Get<bool>("userIsEditingAnInputField")}");
-                Logger.Log($"showingPopup: {scnEditor.instance.Get<bool>("showingPopup")}");
-            }
         }
     }
 }
